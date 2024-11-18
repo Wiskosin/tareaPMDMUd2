6282de6a8622f2bcc4a69c6bd1828ac3ee51330a
ABSerran <adri.b.serran@gmail.com>
2024-11-18 22:01:00 +0100

    add app bar and recycler view layouts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,6 +36,7 @@ dependencies {
     implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
+    implementation(libs.fragment.ktx)
     implementation(libs.constraintlayout)
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
