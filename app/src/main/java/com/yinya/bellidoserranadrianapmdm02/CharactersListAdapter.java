9343759b703f2674fb741ab8232b5f7808764ae2
ABSerran <adri.b.serran@gmail.com>
2024-11-21 20:18:20 +0100

    Create onClick event and details layout

--- app/src/main/java/com/yinya/bellidoserranadrianapmdm02/CharactersListAdapter.java
+++ app/src/main/java/com/yinya/bellidoserranadrianapmdm02/CharactersListAdapter.java
@@ -1,15 +1,18 @@
 package com.yinya.bellidoserranadrianapmdm02;
 
 import android.content.Context;
+import android.telephony.CarrierConfigManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.card.MaterialCardView;
 import com.yinya.bellidoserranadrianapmdm02.databinding.CardCharactersListItemBinding;
 
 import java.util.ArrayList;
@@ -42,17 +45,23 @@ public class CharactersListAdapter extends RecyclerView.Adapter<CharactersListAd
         CharacterCardData currentCharacter = characters.get(position);
         holder.characterName.setText(currentCharacter.getCharacterName());
         holder.characterImage.setImageResource(currentCharacter.getImage());
-
+        View.OnClickListener onClickListener = view -> {
+            // Action to be performed on click event
+            Toast.makeText(view.getContext(), "Elemento clickeado", Toast.LENGTH_SHORT).show();
+        };
+        holder.characterCard.setOnClickListener(onClickListener);
     }
 
     public class CharacterViewHolder extends RecyclerView.ViewHolder {
         private ImageView characterImage;
         private TextView characterName;
+        private MaterialCardView characterCard;
 
         public CharacterViewHolder(@NonNull CardCharactersListItemBinding binding) {
             super(binding.getRoot());
             characterImage = binding.ivCharactersListCardView;
             characterName = binding.tvCharactersListCardView;
+            characterCard = binding.cvCharactersList;
         }
     }
 
